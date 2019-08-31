1204fff2a032c771ec3b1745839627295de6f895
Ariful <arifsofg@gmail.com>
2019-08-31 17:01:19 +0600

    change some files

--- application/config/config.php
+++ application/config/config.php
@@ -2,7 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$config['base_url'] = 'http://localhost/OpenBlog';
+$base_url = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$base_url .= "://". @$_SERVER['HTTP_HOST'];
+$base_url .=     str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $base_url;
 
 
 $config['index_page'] = '';
